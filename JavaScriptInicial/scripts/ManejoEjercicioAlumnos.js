81605148c00e598b9c6cbc749bea88e5794f8d22
RosaMLosa <Rosa.Losa@tajamar365.com>
2014-10-03 09:07:43 +0200

    guardado 2oct2014

---
+++ JavaScriptInicial/scripts/ManejoEjercicioAlumnos.js
@@ -0,0 +1,54 @@
+﻿
+
+var miArrayAlumnos=ArrayAlumnos[];
+function crearAlumno(nombre, nota, edad) {
+    this.nombre = nombre;
+    this.nota = nota;
+    this.edad = edad;
+   
+
+    this.guardarAlumno= function(){
+        miArrayAlumnos.push();
+
+    }
+    this.escribirAlumnosEnListado = function () {
+
+        document.write("* "+this.nombre+"<BR>"); 
+
+    }
+    this.recuperarAlumnosBusqueda=function(){
+        alert("Nombre: "+this.nombre+"<BR>"+"Nota: "+this.nota+"<BR>"+"Edad: "+this.edad);
+    }
+    this.buscarAlumno=function(){
+        
+        var buscarlos = document.getElementById("nombre").value;
+        for(i=0;i<ArrayAlumnos.length;i++){
+            if(ArrayAlumnos[i].nombre==alumno){
+                buscarAlumno.recuperarAlumnosBusqueda;
+                //var alumno=getElementById(ArrayAlumnos[i]);
+            }
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+function calcularMedia(){
+    var media=0;
+    for(i=0;i<ArrayAlumnos.length;i++){
+        media+=this.nota/ArrayAlumnos.length;
+    }
+
+}
+
+function buscarAlumno() {
+    var alumnoBuscado = document.getElementById(this.alumno);
+    capa.innerHTML = "Estoy escribiendo dentro de la capa 1 <br />";       //innerHTML define el html que esta contenido en un contenedor
+
+}
---
+++ JavaScriptInicial/scripts/manejoDocumento.js
@@ -0,0 +1,27 @@
+﻿
+function cambiarVisibilidad() {
+    var capas = document.getElementsByTagName("div");
+
+    for (var i = 0; i < capas.length; i++) {
+        if (capas[i].style.display == "none") {
+            capas[i].style.display = "block";
+        } else {
+            capas[i].style.display = "none";
+        }
+    }
+
+}
+
+function escribirTexto() {
+    var capa = document.getElementById("capa1");
+    capa.innerHTML += "Estoy escribiendo dentro de la capa 1 <br />";       //innerHTML define el html que esta contenido en un contenedor
+
+}
+
+function cambiarPosicion() {            //para que chute necesita que metas un valor en la cja de texto
+    var texto = document.getElementById("posicion").value;
+    var capa = document.getElementById("capa2");
+    if (!isNaN(texto)) {
+        capa.style.left = texto + "px";
+    }
+}
